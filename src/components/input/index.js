80404469e6ad63654de1875b2cc20abf0764862b
unknown <kurdukovr2564@gmail.com>
2021-10-08 00:43:34 +0300

    Added animation of deleting input

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -5,22 +5,48 @@ import store from "../../store";
 import './input.css'
 
 export default class Input extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            classList: "input-wrapper input-wrapper_showed"
+        }
+    }
+
     onInput = (e) => {
         const value = e.target.value;
         store.dispatch(updateInputValue({id: this.props.id, value}));
+    }
+
+    hideInput = () => {
+        const { id } = this.props;
+
+        this.setState({classList: "input-wrapper input-wrapper_hiden"});
+        setTimeout(() => {
+            store.dispatch(removeInput(id))
+        }, 1000)
     }
 
     render() {
         const {id, arr} = this.props;
         
         if(arr.length < 2) {
-            return <input onInput={this.onInput} id={id} className="input" type="text" placeholder="Enter some ingredient"/>
+            return <input 
+                    onInput={this.onInput} 
+                    id={id} 
+                    className="input"
+                    type="text" 
+                    placeholder="Enter some ingredient"/>
         }
         return (
-            <div className="input-wrapper">
-                <input onInput={this.onInput} id={id} className="input" type="text" placeholder="Enter some ingredient"/>
+            <div className={this.state.classList}>
+                <input 
+                    onInput={this.onInput} 
+                    id={id} className="input" 
+                    type="text" 
+                    placeholder="Enter some ingredient"
+                />
                 <div 
-                    onClick={()=>store.dispatch(removeInput(id))} 
+                    onClick={this.hideInput}  
                     className="input-wrapper__button button" 
                     id="remove-input">
                     -
